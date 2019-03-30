48c635ff586143eb0af68f5b9a87cf20508ede03
Alex Michael Powers <44716363+itsnotaboutthecell@users.noreply.github.com>
2019-03-29 19:03:21 -0500

    Reload and Bulk Scripting Logic Added
    Added prompts and created a function to create the final string needed for Azure SQL DW tables.

--- Azure SQL DW Bulk Loader.py
+++ Azure SQL DW Bulk Loader.py
@@ -5,30 +5,47 @@
     Contact: alexmpowers@itsnotaboutthecell.com
 '''
 
+def stringBuilder(partitionData, ID, ServerTableName, timeStamp = None, startYear = None, endYear = None):
+
+    finalPath = ',' + '\'' + ServerTableName + '\',\'' + 'SELECT * FROM ' + ServerTableName + '\''
+    if partitionData == 'n':
+        print(ID + finalPath)
+    else:
+        concatPath = ''
+        for i in range(int(startYear), int(endYear) + 1):
+            for j in range(1,13):
+                fullPath = str(ID) + finalPath + ' WHERE EXTRACT(YEAR FROM ' + timeStamp + ') = ' + str(i) + ' AND EXTRACT(MONTH FROM ' + timeStamp + ') = ' + str(j) + '\',\'' + 'N\'' + ' UNION ALL'
+                concatPath = concatPath + '\n' + fullPath
+                ID = int(ID) + 1
+
+    print(concatPath[:-10])
+
+partitionOption = input('Does the dataset need partitioned? (Y/N)\n').lower()
+while not partitionOption in ('y','n'):
+    partitionOption = input('Please enter the single character (Y/N).\nDoes the dataset need partitioned? (Y/N)\n').lower()
+
 partitionID = input('Starting Partition ID?\n')
 while not partitionID.isnumeric():
     partitionID = input('Please enter a numeric value.\nStarting Partition ID?\n')
 
-sqlTableName = input ('Server and Table Name?\n')
-while not '.' in sqlTableName:
-    sqlTableName = input ('Please include a period between server and table name.\nServer and Table Name?\n')
+sqlServerName = input ('Server Name?\n')
+sqlTableName = input ('Table Name?\n')
 
-timeStamp = input ('TimeStamp Field?\n')
+serverAndTable = (sqlServerName + '.' + sqlTableName).upper()
+
+if partitionOption == 'y':
+    timeStamp = input ('TimeStamp Field?\n')
+    startYear = input('Start Year?\n')
+    while not startYear.isnumeric() or not len(startYear) == 4:
+        startYear = input('Please enter the year in YYYY format.\nStart Year?\n')
     
-startYear = input('Start Year?\n')
-while not startYear.isnumeric() or not len(startYear) == 4:
-    startYear = input('Please enter the year in YYYY format.\nStart Year?\n')
+    endYear = input('End Year?\n')
+    while not endYear.isnumeric() or not len(endYear) == 4:
+        endYear = input('Please enter the year in YYYY format.\nEnd Year?\n')
+
+    stringBuilder(partitionOption, str(partitionID), serverAndTable, timeStamp, startYear, endYear)
     
-endYear = input('End Year?\n')
-while not endYear.isnumeric() or not len(endYear) == 4:
-    endYear = input('Please enter the year in YYYY format.\nEnd Year?\n')
-
-finalPath = ''
-for i in range(int(startYear), int(endYear) + 1):
-    for j in range(1,13):
-        fullPath = str(partitionID) + ',' + '\'' + sqlTableName + '\',\'' + 'SELECT * FROM ' + sqlTableName + ' WHERE EXTRACT(YEAR FROM ' + timeStamp + ') = ' + str(i) + ' AND EXTRACT(MONTH FROM ' + timeStamp + ') = ' + str(j) + '\'' + ' UNION ALL'
-        finalPath = finalPath + '\n' + fullPath
-        partitionID = int(partitionID) + 1
-
-print(finalPath[:-10])
+else:
+    stringBuilder(partitionOption, partitionID, serverAndTable)
+    
 input("Press Enter to exit...")
